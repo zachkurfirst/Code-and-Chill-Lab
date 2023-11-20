171f3117b83f0222948611f83bcc65da3d35566b
Zach Kurfirst <zachmk11@gmail.com>
2023-11-20 11:30:57 -0500

    implement index functionality; redirect create to index view

--- code-and-chill-lab/controllers/restaurants.js
+++ code-and-chill-lab/controllers/restaurants.js
@@ -1,20 +1,35 @@
-const Restaurant = require('../models/restaurant')
+const Restaurant = require("../models/restaurant");
 
 module.exports = {
-    new: newRestaurant,
-    create
-}
+  new: newRestaurant,
+  create,
+  index,
+};
 
 function newRestaurant(req, res) {
-    res.render('restaurants/new', { title:"Add Restaurant", errorMsg: ''})
+  res.render("restaurants/new", { title: "Add Restaurant", errorMsg: "" });
 }
 
-async function create(req,res) {
-    try {
-        const restaurant = await Restaurant.create(req.body)
-        console.log(restaurant)
-        res.send('This worked!')
-    } catch(err) {
-        console.log('create error', err)
-    }
+async function create(req, res) {
+  // if yearOpened is null, delete and reference default value from model
+  for (let key in req.body) {
+    if (req.body[key] === "") delete req.body[key];
+  }
+  try {
+    const restaurant = await Restaurant.create(req.body);
+    console.log(restaurant);
+    res.redirect("/restaurants");
+  } catch (err) {
+    console.log("create error", err);
+    res.render("restaurants/new", { errorMsg: err.message });
+  }
+}
+
+async function index(req, res) {
+  try {
+    const restaurants = await Restaurant.find().sort("yearOpened");
+    res.render("restaurants/index", { title: "All Restaurants", restaurants });
+  } catch (err) {
+    console.log("index error", err);
+  }
 }
--- code-and-chill-lab/routes/restaurants.js
+++ code-and-chill-lab/routes/restaurants.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const restaurantsCtrl = require('../controllers/restaurants')
 
-// router.get('/', restaurantsCtrl.index)
+router.get('/', restaurantsCtrl.index)
 
 router.get('/new', restaurantsCtrl.new)
 
